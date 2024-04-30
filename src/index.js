144776dec270186e1c7ae7f7bea66eea37920411
Joselu <joseluissgz97@gmail.com>
2024-04-30 17:27:44 +0200

    add: return occupancyPercentage rounded

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const bookings = [
 	{
-		checkIn: "2024-04-21",
-		checkOut: "2024-04-21",
+		checkIn: "2024-04-15",
+		checkOut: "2024-04-23",
 	},
 	{
-		checkIn: "2024-04-22",
-		checkOut: "2024-04-23",
+		checkIn: "2024-04-24",
+		checkOut: "2024-04-26",
 	},
 	{
 		checkIn: "2024-03-10",
@@ -20,6 +20,15 @@ class Room {
 		this.rate = rate;
 		this.discount = discount;
 	}
+	isOccupied(date) {
+		const dateToCheck = new Date(date).getTime();
+		const isOccupied = this.bookings.some((booking) => {
+			const bookingDate = new Date(booking.checkIn).getTime();
+			return dateToCheck === bookingDate;
+		});
+
+		return isOccupied;
+	}
 	occupancyPercentage(startDate, endDate) {
 		const startDatee = new Date(startDate).getTime();
 		const endDatee = new Date(endDate).getTime();
@@ -31,23 +40,27 @@ class Room {
 			const bookingStart = new Date(booking.checkIn).getTime();
 			const bookingEnd = new Date(booking.checkOut).getTime();
 
-			// Verificar si alguna parte de la reserva coincide con el período evaluado
 			if (startDatee <= bookingEnd && endDatee >= bookingStart) {
-				// Calcular la cantidad de días ocupados durante la reserva
 				const overlapStart = Math.max(startDatee, bookingStart);
+
 				const overlapEnd = Math.min(endDatee, bookingEnd);
+
 				const overlapDays =
 					(overlapEnd - overlapStart) / (1000 * 60 * 60 * 24) + 1;
 
-				// Incrementar los días ocupados
 				daysOccupied += overlapDays;
 			}
 		});
 		const occupancyPercentage = (daysOccupied / totalDays) * 100;
-		return { occupancyPercentage, daysOccupied, totalDays };
+		// return { occupancyPercentage, daysOccupied, totalDays };
+		return Math.ceil(occupancyPercentage);
 	}
 }
 
 const room = new Room({ name: "101", bookings, rate: 100, discount: 10 });
 
-console.log(room.occupancyPercentage("2024-04-21", "2024-04-26"));
+console.log(room.occupancyPercentage("2024-04-21", "2024-04-23"));
+
+console.log(room.isOccupied("2024-04-24"));
+
+module.exports = Room;
