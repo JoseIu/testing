1eb5ea6a1b94332fdabd73694450933d316c1aef
Joselu <joseluissgz97@gmail.com>
2024-05-06 19:53:06 +0200

    add: RoomProperties, RoomMethods and RoomInterface

--- src/interfaces/Room.interface.ts
+++ src/interfaces/Room.interface.ts
@@ -1,6 +1,16 @@
-export interface RoomInterface {
+import { BookingInterface } from "./Booking.Interface";
+
+export interface RoomProperties {
 	name: string;
 	rate: number;
 	discount: number;
-	bookings?: [];
+	bookings?: BookingInterface[];
+}
+interface RoomMethods {
+	setBookings: (bookings: BookingInterface[]) => void;
+	isOccupied: (date: string) => boolean | void;
+	occupancyPercentage: (startDate: string, endDate: string) => number;
+}
+export interface RoomInterface extends RoomProperties, RoomMethods {
+	occupancyPercentage: (startDate: string, endDate: string) => number;
 }
