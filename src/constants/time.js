10e0ca64d0c7dc83d5e558c05daba29fa4e8e3d7
Joselu <joseluissgz97@gmail.com>
2024-05-06 20:45:06 +0200

    add: change Time to ts

--- src/constants/time.js
+++
@@ -1,8 +0,0 @@
-const TIME = {
-	SECOND: 1000,
-	MINUTE: 60,
-	HOUR: 60,
-	DAY: 24,
-};
-
-module.exports = TIME;
---
+++ src/constants/time.ts
@@ -0,0 +1,15 @@
+interface Time {
+	SECOND: number;
+	MINUTE: number;
+	HOUR: number;
+	DAY: number;
+}
+
+const TIME: Time = {
+	SECOND: 1000,
+	MINUTE: 60,
+	HOUR: 60,
+	DAY: 24,
+};
+
+export default TIME;
